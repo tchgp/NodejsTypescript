f84d2bddc2ba53b03d46227172014250a28baaad
tchgp <tchipoura@gmail.com>
2015-11-03 20:25:52 +0300

    New commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp'), 
+    ts = require('gulp-typescript');
+
+gulp.task('default', function () {
+    console.log('Доступные модули:\n' + 
+    			'- "tsc:commonjs"\n' +
+                '- "watch"\n');
+});
+
+gulp.task('tsc:commonjs', function () {
+    console.log('Compiling typescript');
+    return gulp.src(['src/**/*.ts'])
+        .pipe(ts({module: 'commonjs'}))
+        .js.pipe(gulp.dest('dist/'))
+});
+
+gulp.task('watch', function(){
+  // Наблюдение за .ts файлами
+  gulp.watch('src/**/*.ts', ['tsd:commonjs']);
+});
